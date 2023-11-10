f3418d096024657c25e7c2a1252f68446d2a105c
TommyVS1258 <vuhvijay336@gmail.com>
2023-11-10 14:49:39 +0530

    add: serviceCard component updated

--- src/components/Services/ServicesList.jsx
+++ src/components/Services/ServicesList.jsx
@@ -5,7 +5,7 @@ const ServicesList = () => {
    return (
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 mt-[2rem] lg:mt-[3rem]'>
          {services.map((item, index) => (
-            <ServicesCard item={item} key={index} />
+            <ServicesCard item={item} index={index} key={index} />
          ))}
       </div>
    );
